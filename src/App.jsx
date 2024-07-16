20e52696d1910a3fefe52300b4d1fb6d2e03463b
Afiddrus <afid123456@gmail.com>
2024-07-16 21:52:55 +0700

    create loading with react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import {requestToGroqAI} from "./utils/groq";
 import {Light as SyntaxHighLight} from "react-syntax-highlighter";
 // import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
-
+import {FaSpinner} from "react-icons/fa";
 function App() {
   const [data, setData] = useState("")
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async() => {
+    setLoading(true);
     const ai = await requestToGroqAI(content.value);
     setData(ai);
+    setLoading(false);
   };
+
   return (
     <main className='flex flex-col min-h-[80vh] justify-center items-center max-w-xl w-full mx-auto'>
       <h1 className='text-4xl text-indigo-500'>REACT|GROQ AI</h1>
@@ -25,8 +29,9 @@ function App() {
         <button
         onClick={handleSubmit} 
         type='button'
-        className='bg-indigo-500 py-2 px-4 font-bold text-white rounded-md'>
-          Kirim
+        className={`flex items-center justify-center gap-2 ${loading ? 'bg-gray-400' : 'bg-indigo-500'} py-2 px-4 font-bold text-white rounded-md ${loading ? 'cursor-not-allowed' : ''}`}
+        disabled={loading}>
+          {loading ? <FaSpinner className="animate-spin" /> : 'Kirim'}
         </button>
       </form>
       <div className="max-w-xl w-full mx-auto">
