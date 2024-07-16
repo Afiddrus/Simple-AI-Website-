0e131886b66874ddcbef8ec981b8902538edab85
Afiddrus <afid123456@gmail.com>
2024-07-16 17:13:15 +0700

    showing result with react-syntax-highlighter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import  {useState} from "react";
 import './App.css'
 import {requestToGroqAI} from "./utils/groq";
+import {Light as SyntaxHighLight} from "react-syntax-highlighter";
+// import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 function App() {
   const [data, setData] = useState("")
@@ -10,9 +13,9 @@ function App() {
     setData(ai);
   };
   return (
-    <main className='flex flex-col min-h-[80vh] justify-center items-center'>
+    <main className='flex flex-col min-h-[80vh] justify-center items-center max-w-xl w-full mx-auto'>
       <h1 className='text-4xl text-indigo-500'>REACT|GROQ AI</h1>
-      <form className='flex flex-col gap-4 py-4'>
+      <form className='flex flex-col gap-4 py-4 w-full'>
         <input 
         placeholder='Ketik Permintaan Disini....'
         className='py-2 px-4 text-md rounded-md'
@@ -26,7 +29,11 @@ function App() {
           Kirim
         </button>
       </form>
-      <div>{data}</div>
+      <div className="max-w-xl w-full mx-auto">
+        {data ? (
+      <SyntaxHighLight language="swift" style={darcula} wrapLongLines="true">{data}</SyntaxHighLight>
+        ) : null}
+      </div>
     </main>
   )
 }
